3b8f53f35b35c68535160042517656afc1808b8c
Ralph Florent <ralflornt@gmail.com>
2021-03-07 12:21:38 -0500

    feat: add cache support to config

--- lib/src/config.dart
+++ lib/src/config.dart
@@ -49,13 +49,27 @@ class Config {
   /// namefully.
   LastNameFormat lastNameFormat;
 
-  /// Cache of an instance of this class (self instantiation with default props).
-  static final Config _config = Config._internal();
+  /// The name of the cached [Config].
+  final String name;
+
+  /// Cache of multiple instances
+  static final Map<String, Config> _cache = {
+    'default': Config._default('default')
+  };
 
   /// Returns a single [Config] with default values.
-  factory Config() => _config;
+  factory Config([String name = 'default']) {
+    if (_cache.containsKey(name)) {
+      return _cache[name]!;
+    } else {
+      final config = Config._default(name);
+      _cache[name] = config;
+      return config;
+    }
+  }
 
-  Config._internal()
+  /// Self instantiation with default props.
+  Config._default(this.name)
       : orderedBy = NameOrder.firstName,
         separator = Separator.space,
         titling = AbbrTitle.uk,
@@ -68,29 +82,33 @@ class Config {
   /// optional values to consider.
   ///
   /// This allows an override of some properties
-  factory Config.inline(
-      {NameOrder? orderedBy,
-      Separator? separator,
-      AbbrTitle? titling,
-      bool? ending,
-      bool? bypass,
-      Parser<dynamic>? parser,
-      LastNameFormat? lastNameFormat}) {
-    _config.orderedBy = orderedBy ?? NameOrder.firstName;
-    _config.separator = separator ?? Separator.space;
-    _config.titling = titling ?? AbbrTitle.uk;
-    _config.ending = ending ?? false;
-    _config.bypass = bypass ?? false;
-    _config.parser = parser;
-    _config.lastNameFormat = lastNameFormat ?? LastNameFormat.father;
-    return _config;
+  factory Config.inline({
+    String name = 'default',
+    NameOrder? orderedBy,
+    Separator? separator,
+    AbbrTitle? titling,
+    bool? ending,
+    bool? bypass,
+    Parser<dynamic>? parser,
+    LastNameFormat? lastNameFormat,
+  }) {
+    var config = Config(name);
+    config.orderedBy = orderedBy ?? NameOrder.firstName;
+    config.separator = separator ?? Separator.space;
+    config.titling = titling ?? AbbrTitle.uk;
+    config.ending = ending ?? false;
+    config.bypass = bypass ?? false;
+    config.parser = parser;
+    config.lastNameFormat = lastNameFormat ?? LastNameFormat.father;
+    return config;
   }
 
   /// Returns a unified version of prexisting values of [Config] and the [other]
   /// provided values.
-  factory Config.mergeWith(Config? other) {
-    if (other == null) return _config;
+  factory Config.mergeWith(Config? other, {String name = 'default'}) {
+    if (other == null) return Config(name);
     return Config.inline(
+        name: name,
         orderedBy: other.orderedBy,
         separator: other.separator,
         titling: other.titling,
