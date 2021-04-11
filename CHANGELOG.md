77316733a994eacea7e7c40a3b78011df13c9165
Ralph Florent <ralflornt@gmail.com>
2021-04-10 22:06:58 -0400

    build: stable version 0.1.1 release

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,10 +1,17 @@
-# 0.1.0+1
+# 0.1.1
+
+* Extract a `Summarizable` mixin from `Summary`
+* Add `raw` constructor to `FullName`
+* Add support for name flattening with or without period
+* Add copy capabilities to `Config`
+
+## 0.1.0+1
 
 * Fix badges in README.
 
 ## 0.1.0
 
-* First sable release.
+* First stable release.
 
 ## 0.1.0-beta.1
 
--- example/namefully.dart
+++ example/namefully.dart
@@ -1,8 +1,26 @@
 import 'package:namefully/namefully.dart';
 
 void main() {
+  // Gives a simple name super power.
   var name = Namefully('Jon Stark Snow');
+  // Gets the count of characters, excluding space.
+  print(name.count); // 12
+  // Gets the count of characters, including space.
+  print(name.length); // 14
+  // Gets the first name.
+  print(name.firstName()); // Jon
+  // Gets the first name.
+  print(name.lastName()); // Snow
+  // Gets all the initials.
+  print(name.initials(withMid: true)); // ['J', 'S', 'S']
+  // Formats as desired.
   print(name.format('L, f m')); // SNOW, Jon Stark
+  // Make it short.
   print(name.shorten()); // Jon Snow
+  // Make it flat.
   print(name.zip()); // Jon S. S.
+  // Make it uppercase.
+  print(name.upper()); // JON STARK SNOW
+  // Transforms it to dot.case.
+  print(name.dot()); // jon.stark.snow
 }
